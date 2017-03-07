5efa7ae1578ba92dc59df5725d1ceb947eb77c66
davidmulligan <davidmulligan@btopenworld.com>
2017-03-07 12:11:44 +0000

    wip

--- code/web-console/src/main/resources/static/app/factories/jobs.factory.js
+++ code/web-console/src/main/resources/static/app/factories/jobs.factory.js
@@ -7,11 +7,26 @@
 
     /* @ngInject */
     function factory($resource, JOBS_URL) {
-         return $resource(JOBS_URL, { id: '@_id' }, {
+        return $resource(JOBS_URL, { id: '@_id' }, {
             self: {
                 method: 'GET',
                 url: JOBS_URL + "/self",
                 isArray: true
+            },
+            placeBid: {
+                method: 'POST',
+                url: JOBS_URL + '/:id/bids',
+                params: {id: '@id'}
+            },
+            cancel: {
+                method: 'PUT',
+                url: JOBS_URL + '/:id/cancel',
+                params: {id: '@id'}
+            },
+            accept: {
+                method: 'PUT',
+                url: JOBS_URL + '/:id/bids/:bidId/accept',
+                params: {id: '@id', bidId: '@bidId'}
             }
         });
     }
--- code/web-console/src/main/resources/static/app/sections/client/account/jobs/jobs.controller.js
+++ code/web-console/src/main/resources/static/app/sections/client/account/jobs/jobs.controller.js
@@ -6,7 +6,7 @@
         .controller('ClientAccountJobsController', controller);
 
     /** @ngInject */
-    function controller($uibModal, NotifyService, Jobs) {
+    function controller($uibModal, $log, NotifyService, Jobs) {
         var vm = this;
 
         vm.init = function() {
@@ -15,7 +15,7 @@
             })
         };
 
-        vm.modalDetail = function(selectedJob) {
+        vm.modalJobDetail = function(selectedJob) {
 
             var modalInstance = $uibModal.open({
                 animation: true,
@@ -44,13 +44,52 @@
             });
         };
 
-        vm.cancel = function(job) {
+        vm.modalFreelancerDetail = function(selectedBid, selectedJob) {
+
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'app/sections/client/account/jobs/freelancer_detail.html',
+                controller: function($scope, $uibModalInstance, $log, bid, job) {
+                    $scope.bid = bid;
+                    $scope.job = job;
+
+                    $scope.ok = function() {
+                        $uibModalInstance.close($scope.bid);
+                    };
+
+                    $scope.cancel = function() {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'lg',
+                resolve: {
+                    bid: function() {
+                        return selectedBid;
+                    },
+                    job: function() {
+                        return selectedJob;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(selectedItem) {}, function() {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        vm.acceptBid = function(bid, job) {
+            Jobs.accept({id: job.id, bidId: bid.id}, function(result) {
+                NotifyService.sendMessage('JobChangeEvent');
+            });
+        };
+
+        vm.cancelJob = function(job) {
             Jobs.cancel({id: job.id}, function(result) {
                 NotifyService.sendMessage('JobChangeEvent');
             });
         };
 
-        vm.archive = function(job) {
+        vm.archiveJob = function(job) {
             Jobs.cancel({id: job.id}, function(result) {
                 NotifyService.sendMessage('JobChangeEvent');
             });

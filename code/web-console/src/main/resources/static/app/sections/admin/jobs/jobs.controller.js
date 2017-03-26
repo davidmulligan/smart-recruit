2d28a39373365ca4f3fc52be8909f01901ff8f00
davidmulligan <davidmulligan@btopenworld.com>
2017-03-26 03:22:21 +0100

    wip

--- code/web-console/src/main/resources/static/app/sections/admin/jobs/jobs.controller.js
+++ code/web-console/src/main/resources/static/app/sections/admin/jobs/jobs.controller.js
@@ -6,21 +6,50 @@
         .controller('AdminJobsController', controller);
 
     /** @ngInject */
-    function controller($log, ngToast, Jobs, NotifyService) {
+    function controller($uibModal, $log, ngToast, Jobs, NotifyService) {
         var vm = this;
 
         vm.init = function() {
             vm.fetchJobs();
         };
 
-       vm.fetchJobs = function() {
-           Jobs.getAll({'status':'NEW'},
-               function(data) {
-                  vm.jobs = data;
-                  $log.info('Successfully fetched ' + data.length + ' jobs.');
-               }
-           );
-       };
+        vm.fetchJobs = function() {
+            Jobs.getAll({'status':'NEW'},
+                function(data) {
+                    vm.jobs = data;
+                    $log.info('Successfully fetched ' + data.length + ' jobs.');
+                }
+            );
+        };
+
+        vm.modalJobDetail = function(selectedJob) {
+
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'app/sections/admin/jobs/job_detail.html',
+                controller: function($scope, $uibModalInstance, $log, job) {
+                    $scope.job = job;
+
+                    $scope.ok = function() {
+                        $uibModalInstance.close($scope.job);
+                    };
+
+                    $scope.cancel = function() {
+                        $uibModalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'lg',
+                resolve: {
+                    job: function() {
+                        return selectedJob;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(selectedItem) {}, function() {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
 
         vm.approve = function(job) {
             Jobs.approve({id: job.id}, function(result) {
